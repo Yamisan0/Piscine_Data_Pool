9978ad21c488c72cb48497e6d4c81d1cc540d4e1
Yamisan0 <idris.messaad@gmail.com>
2024-03-01 16:13:54 +0100

    feat: ex03

---
+++ ex03/NULL_not_found.py
@@ -0,0 +1,15 @@
+def NULL_not_found(object: any) -> int:
+    if (object == None):
+        print("Nothing: None", type(object))
+    elif (isinstance(object, int) and object == 0) :
+        print("Zero: 0", type(object))
+    elif (isinstance(object, float) and object != object):
+        print("Cheese: nan", type(object))
+    elif (isinstance(object, str) and len(object) == 0):
+        print("Empty:", type(object))
+    elif (isinstance(object, bool) and object == False):
+        print("Fake: False", type(object))
+    else:
+        print("Type not Found")
+        return 1
+    return 0
